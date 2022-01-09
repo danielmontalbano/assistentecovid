386e4594c2c3cc369ce771923cbcdcf7da63df9b
daniel <dani.montalbano2000@gmail.com>
2022-01-10 00:05:08 +0100

    logo

--- components/layout.js
+++ components/layout.js
@@ -14,7 +14,7 @@ export default function Layout({ children, home }) {
   return (
     <div className="flex flex-col">
       <Head>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/logoac.ico" />
         <meta
           name="description"
           content="Learn how to build a personal website using Next.js"
