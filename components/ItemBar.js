0d3bfa0e76ab9de19c5e5bf3f6e881721f512144
daniel <dani.montalbano2000@gmail.com>
2022-01-09 14:55:35 +0100

    fix width

--- components/ItemBar.js
+++ components/ItemBar.js
@@ -1,7 +1,7 @@
 export default function ItemBar({ children }){
     return (
         <div className="container mx-auto">
-            <div className="flex flex-col text-center w-full m-2">
+            <div className="text-center flex flex-col m-2">
                 {children}
             </div>
         </div>
