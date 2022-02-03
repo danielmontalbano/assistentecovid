e7f313383897160a6ffc6fd9342bf5804baf9fd5
daniel <dani.montalbano2000@gmail.com>
2022-02-03 16:44:41 +0100

    text

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,11 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Navbar(){
     return (
         <nav className="flex items-center justify-between flex-wrap bg-teal p-6 bg-green0">
   <div className="flex items-center flex-no-shrink text-white mr-6">
-  <Image src="/images/logo.png" alt="Hello" width={200} height={50} />
-    
+    <Link href='/'>
+        <Image src="/images/logo.png" alt="assisstente covid" width={200} height={50} className="cursor-pointer" />
+    </Link>
   </div>
 </nav>
     );
